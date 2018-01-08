a94e6da6b61e438d5cc6c5875a6e9059812dfa01
trilliams <tribecca@tribecc.us>
2018-01-08 16:05:06 -0600

    still padding out stuff

--- controller/entrypoint.sh
+++ controller/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-# mysql setup on controller node
+# credentials for env setup
+export OS_USERNAME=admin
+export OS_PASSWORD=openstack
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://controller:35357/v3
+export OS_IDENTITY_API_VERSION=3
 
+# mysql setup on controller node
+echo "Starting MariaDB service..."
+systemctl restart mysql
 # rabbitmq
+echo "Starting RabbitMQ service..." 
+systemctl restart rabbitmq
 rabbitmqctl add_user openstack openstack
 sleep 3
 rabbitmqctl set_permissions openstack ".*" ".*" ".*"
@@ -16,25 +28,44 @@ chown -R etcd:etcd /etc/etcd
 mkdir -p /var/lib/etcd
 chown -R etcd:etcd /var/lib/etcd
 
-ETCD_VER=v3.2.7
-rm -rf /tmp/etcd && mkdir -p /tmp/etcd
-curl -L https://github.com/coreos/etcd/releases/download/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
-tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd --strip-components=1
+mkdir -p /tmp/etcd
+tar xzvf /tmp/etcd-v3.2.7-linux-amd64.tar.gz -C /tmp/etcd --strip-components=1
 cp /tmp/etcd/etcd /usr/bin/etcd
 cp /tmp/etcd/etcdctl /usr/bin/etcdctl
+chown -R nova:nova /etc/nova
+chown -R root:glance /etc/glance
+chown -R keystone:keystone /etc/keystone
+chown -R horizon:horizon /var/lib/horizon
 
- # keystone
+# keystone
 echo "create database keystone;" | mysql
 echo "grant all privileges on keystone.* to 'keystone'@'localhost' identified by 'openshift';" | mysql
 echo "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'openshift';" | mysql
 echo "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'openshift';" | mysql
 echo "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'openshift';" | mysql
+
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap --bootstrap-password openstack \
+     --bootstrap-admin-url http://controller:35357/v3/ \
+     --bootstrap-internal-url http://controller:5000/v3/ \
+     --bootstrap-public-url http://controller:5000/v3/ \
+     --bootstrap-region-id RegionOne
+# endpoint setup
+openstack project create --domain default --description "Service Project" service
+openstack project create --domain default --description "Demo Project" demo
+openstack user create --domain default --password openstack demo
+openstack role create user
+openstack role add --project demo --user demo user
 # glance
 
 echo "create database glance;" | mysql
 echo "grant all privileges on glance.* to 'glance'@'localhost' identified by 'openshift';" | mysql
 echo "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY 'openshift';" | mysql
-chown -R root:glance /etc/glance
+su -s /bin/sh -c "glance-manage db_sync" glance
+service glance-registry restart
+service glance-api restart
 
 # nova
 echo "create database nova_api;" | mysql
@@ -46,23 +77,40 @@ echo "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'openst
 echo "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'openstack';" | mysql
 echo "GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'localhost' IDENTIFIED BY 'openstack';" | mysql
 echo "GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'%' IDENTIFIED BY 'openstack';" | mysql
+openstack user create --domain default --password openstack nova
+openstack role add --project service --user nova admin
+openstack service create --name nova --description "OpenStack Compute" compute
+openstack endpoint create --region RegionOne compute public http://controller:8774/v2.1
+openstack endpoint create --region RegionOne compute internal http://controller:8774/v2.1
+openstack endpoint create --region RegionOne compute admin http://controller:8774/v2.1
+openstack user create --domain default --password openstack placement
+openstack role add --project service --user placement admin
+openstack service create --name placement --description "Placement API" placement
+openstack endpoint create --region RegionOne placement public http://controller:8778
+openstack endpoint create --region RegionOne placement internal http://controller:8778
+openstack endpoint create --region RegionOne placement admin http://controller:8778
+
 # neutron
 echo "create database neutron;" | mysql
 echo "GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'localhost' IDENTIFIED BY 'openstack';" | mysql
 echo "GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'%' IDENTIFIED BY 'openstack';" | mysql
 
-# rabbit
-while true; do
-    if [ "$RABBIT_PASS" ]; then
-        rabbitmqctl change_password guest $RABBIT_PASS
-        if [ $? == 0 ]; then break
-        else echo "Waiting for RabbitMQ Server Password change....";sleep 1
-        fi
-    fi
-done
+openstack user create --domain default --password openstack neutron
+openstack role add --project service --user neutron admin
+openstack service create --name neutron --description "OpenStack Networking" network
+openstack endpoint create --region RegionOne network public http://controller:9696
+openstack endpoint create --region RegionOne network internal http://controller:9696
+openstack endpoint create --region RegionOne network admin http://controller:9696
 
+su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf \
+      --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
 
-
+service nova-api restart
+service neutron-server restart
+service neutron-linuxbridge-agent restart
+service neutron-dhcp-agent restart
+service neutron-metadata-agent restart
+service neutron-l3-agent restart
 
 
 while true
